0e8599de27033af63f261cced2d7eb474a4b7208
yongcowhee <yongcowhee@gmail.com>
2026-03-11 02:31:16 +0900

    feat: Mailpit 이메일 인증코드 파싱 로직 구현

--- pages/signup_page.py
+++ pages/signup_page.py
@@ -1,3 +1,7 @@
+import time     # 시간 관련 함수 라이브러리 - 이메일 수신 대기에 사용
+import requests  # HTTP 요청 라이브러리 - Mailpit API 호출에 사용
+import re        # 정규표현식 라이브러리 - 메일 본문에서 인증코드 추출에 사용
+
 from pages.base_page import BasePage
 
 class SignupPage(BasePage):
@@ -17,3 +21,24 @@ class SignupPage(BasePage):
         self.input_username.fill(user_name)
         self.input_email.fill(email)
         self.input_pw.fill(pw)
+
+    def click_auth_email(self):
+        self.auth_email_button.click()
+        time.sleep(2)  # 메일이 Mailpit에 도달할 때까지 2초 대기
+
+    def get_email_auth_code(self, to_email: str):
+        res = requests.get("http://localhost:8025/api/v1/messages")
+        messages = res.json()["messages"]
+
+        assert len(messages) > 0, "❌ Mailpit에 수신된 메일이 없습니다. 이메일 발송이 됐는지 확인해주세요."
+
+        latest_message = messages[0]
+        snippet = latest_message["Snippet"]  # "YOUngChat 인증 코드입니다. CODE : 75a2329b" 형태
+
+        print("📨 수신된 메시지 본문 : " ,snippet)
+
+        code = re.search(r'[a-f0-9]{8}', snippet).group()
+
+        assert code is not None, "❌ 인증코드를 찾을 수 없습니다."
+
+        return code
